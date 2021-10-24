40b3a77ef13c8d66e9dd3bc083f0946387f16256
Kateryna Ueno <katerynaueno@gmail.com>
2021-10-24 10:42:55 +0100

    continuing with react

--- section1/lecture14-props/Card.js
+++ section1/lecture14-props/Card.js
@@ -1,19 +1,21 @@
-function Card(props){
-    console.log(props);
-    return(
-        <div className="col s2">
-            <div className="card hoverable small">
-                <div className="card-image">
-                    <img src="http://lorempixel.com/400/400/nature/" />
-                </div>
-                <div className="card-content">
-                    <p>{props.title}</p>
-                    <p>{props.name}</p>
-                </div>
-                <div className="card-action">
-                    <a href="#">$9.99</a>
-                </div>
-            </div>
-        </div>	
-    )
+function Card(props) {
+  console.log(props);
+  return (
+    <div className="row">
+      <div className="col s2">
+        <div className="card hoverable small">
+          <div className="card-image">
+            <img src="http://lorempixel.com/400/400/nature/" />
+          </div>
+          <div className="card-content">
+            <p></p>
+            <p>Robert Bunch</p>
+          </div>
+          <div className="card-action">
+            <a href="#">$9.99</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- section1/lecture15-practiceProps/app.js
+++ section1/lecture15-practiceProps/app.js
@@ -1,1 +1,33 @@
 // ReactDOM.render here
+ReactDOM.render(
+  <div className="row">
+    <Card data={data[0]} />
+    <Card data={data[1]} />
+    <Card data={data[2]} />
+    <Card data={data[3]} />
+    {/* <Card
+      name={data[0].instructor}
+      title={data[0].course}
+      picture={data[0].image}
+    />
+    ,
+    <Card
+      name={data[1].instructor}
+      title={data[1].course}
+      picture={data[1].image}
+    />
+    ,
+    <Card
+      name={data[2].instructor}
+      title={data[2].course}
+      picture={data[2].image}
+    />
+    ,
+    <Card
+      name={data[3].instructor}
+      title={data[3].course}
+      picture={data[3].image}
+    /> */}
+  </div>,
+  document.getElementById("root")
+);
