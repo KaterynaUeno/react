528e9d44be7386931ce2561f5d868899890f7dd0
Kateryna Ueno <katerynaueno@gmail.com>
2021-10-24 15:20:50 +0100

    Lesson 18 is done

--- section1/lecture18-practiceClassArray/CatNav.js
+++ section1/lecture18-practiceClassArray/CatNav.js
@@ -1,11 +1,21 @@
 // change this Component to a class!
-function CatNav(props){
-    // get data from props and use map to build an array of <li>
-    // ... code goes here
-    return(
-        <div>
-            {/* Your Code Here */}
-        </div>
-    )
+class CatNav extends React.Component {
+  render() {
+    console.log(this.props);
+    const navElements = this.props.data.map((element, index) => {
+      return (
+        <li key={index} className="cat-link left valign-wrapper">
+          <i className="material-icons">{element.icon}</i>
+          {element.title}
+        </li>
+      );
+    });
+    return (
+      <div className="row">
+        <ul className="cat-nav center-align">{navElements}</ul>
+      </div>
+    );
+  }
 }
-
+// get data from props and use map to build an array of <li>
+// ... code goes here
