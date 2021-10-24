4487cb245c197abaae235b29f660c4ff3b65f372
Kateryna Ueno <katerynaueno@gmail.com>
2021-10-24 10:56:38 +0100

    lecture 16 finished

--- section1/lecture16-arrayOfComponents/app.js
+++ section1/lecture16-arrayOfComponents/app.js
@@ -1,14 +1,16 @@
-let cards = data.map((course,i)=>{
-    return(
-        <Card key={i} data={course} />
-    )
-})
-console.log(cards)
-
+let cards = data.map((card, index) => {
+  return <Card key={index} data={card} />;
+});
+console.log(cards);
 // ReactDOM.render here
 ReactDOM.render(
-    <div className="row">
-        {cards}
-    </div>,
-    document.getElementById('root')
-)
+  <div className="row">
+    {cards}
+    {/* <Card data={data[0]} />
+    <Card data={data[1]} />
+    <Card data={data[2]} />
+    <Card data={data[3]} /> */}
+  </div>,
+
+  document.getElementById("root")
+);
