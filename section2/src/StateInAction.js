a960c918da19a81c1d85290513cd027808b7ee21
Kateryna Ueno <katerynaueno@gmail.com>
2021-10-25 11:10:02 +0100

    State beginning

---
+++ section2/src/StateInAction.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class StateInAction extends Component {
+  constructor() {
+    super();
+    // console.log("constructor is running");
+    this.state = {
+      text: "State In Action!",
+    };
+    setTimeout(() => {
+      this.setState({
+        text: "State has changed!",
+      });
+    }, 2000);
+  }
+  render() {
+    return <h1>{this.state.text}</h1>;
+  }
+}
+
+export default StateInAction;
