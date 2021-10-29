3cafe13629cbfe66b7b630e7e7970f1f64ccf500
Kateryna Ueno <katerynaueno@gmail.com>
2021-10-29 12:36:36 +0100

    styles

--- section3/src/temperatureApp/TemperatureInput.js
+++ section3/src/temperatureApp/TemperatureInput.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import "./temperatureInput.css";
 
 class TemperatureInput extends Component {
   render() {
     const temperature = this.props.temperature;
+    let style;
+    let hotClass;
+    if (temperature > 100) {
+      style = {
+        color: "red",
+        backgroundColor: "orange",
+      };
+      hotClass = "too-hot";
+    }
     return (
-      <div>
-        <legend>Enter temperature in {this.props.scale}</legend>
+      <div className="temp-input">
+        <legend className={hotClass}>
+          Enter temperature in {this.props.scale}
+        </legend>
         <input
           value={temperature}
           onChange={(e) => {
