ff8ac8695a1913fe09c595e6ee468efa69e936ad
Kateryna Ueno <katerynaueno@gmail.com>
2021-10-29 12:23:18 +0100

    Lifting state up

--- section3/src/index.js
+++ section3/src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import FormPractice from "./FormPractice";
+// import FormPractice from "./FormPractice";
+import Calculator from "./temperatureApp/Calculator";
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <FormPractice />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 ReactDOM.render(
   <React.StrictMode>
-    <FormPractice />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById("root")
 );
