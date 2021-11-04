ee8e44d304fff93c9d794eaf78b7ff3530b0482f
Kateryna Ueno <katerynaueno@gmail.com>
2021-11-04 10:53:14 +0000

    finished

--- redux-middleware/src/index.js
+++ redux-middleware/src/index.js
@@ -4,10 +4,11 @@ import "./index.css";
 import App from "./App";
 
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers/rootReducer";
+import reduxPromise from "redux-promise;";
 
-const theStore = createStore(rootReducer);
+const theStore = applyMiddleware(reduxPromise)(createStore)(rootReducer);
 
 ReactDOM.render(
   <Provider store={theStore}>
