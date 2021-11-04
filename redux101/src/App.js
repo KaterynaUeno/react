560e56aa842f86a9d44e2243de1e9898dbe29b23
Kateryna Ueno <katerynaueno@gmail.com>
2021-11-04 15:08:56 +0000

    To fix

--- redux101/src/App.js
+++ redux101/src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import FrozenDept from "./components/FrozenDept";
 import MeatDept from "./components/MeatDept";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
+    // <Router>
     <div className="App">
       <FrozenDept />
       <MeatDept />
+      {/* <Route path="/frozen-dept" component={FrozenDept} />
+      <Route path="/meat-dept" component={MeatDept} /> */}
     </div>
+    // </Router>
   );
 }
 
