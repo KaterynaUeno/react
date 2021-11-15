42ca3a5ff8246172bed0b753b2187bd9a79fb0d1
Kateryna Ueno <katerynaueno@gmail.com>
2021-11-15 16:15:58 +0000

    to continue

--- js/src/components/ErrorExample.js
+++ js/src/components/ErrorExample.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 export default function ErrorExample() {
+  let title = "random title";
+  const [text, setText] = useState("Random text");
+  const handleClick = () => {
+    if (text === "Random text") {
+      setText("Random text changed with state");
+    } else {
+      setText("Random text");
+    }
+    // title = "hello people";
+    // console.log(title);
+    //we need to rerender title to display it. Thats why we need use state
+  };
   return (
     <div>
-      <h1>error example</h1>
+      <h2>{text}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Text
+      </button>
     </div>
   );
 }
