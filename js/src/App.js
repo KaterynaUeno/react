0cc175f41cdd46cf1fece83421fb4625db65bd90
Kateryna Ueno <katerynaueno@gmail.com>
2021-11-16 11:12:21 +0000

    Birthdays list

--- js/src/App.js
+++ js/src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
+
+import data from "./data";
+import List from "./componentsProjects/List";
 // import ErrorExample from "./components/ErrorExample";
-import UseStateCounter from "./components/UseStateCounter";
+// import Use StateCounter from "./components/UseStateCounter";
 // import UseStateObject from "./components/UseStateObject";
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
-    <div className="container">
-      {/* <ErrorExample /> */}
-      {/* <UseStateObject /> */}
-      <UseStateCounter />
-    </div>
+    <main>
+      <section className="container">
+        {/* <ErrorExample /> */}
+        {/* <UseStateObject /> */}
+        {/* <UseStateCounter /> */}
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
   );
 }
 
--- js/src/components/UseStateCounter.js
+++ js/src/components/UseStateCounter.js
@@ -3,6 +3,15 @@ import { useState } from "react";
 
 export default function UseStateCounter() {
   const [value, setValue] = useState(0);
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1);
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
   return (
     <div>
       <section style={{ margin: "4rem 0" }}>
@@ -16,6 +25,13 @@ export default function UseStateCounter() {
         </button>
         <button className="btn" onClick={() => setValue(0)}>
           reset
+        </button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          increase later
         </button>
       </section>
     </div>
