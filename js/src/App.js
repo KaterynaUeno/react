789a17f67bcd5ad35b262de6b2117eb0eefd93f3
Kateryna Ueno <katerynaueno@gmail.com>
2021-11-16 14:30:09 +0000

    Alert Dialog from material ui

--- js/src/App.js
+++ js/src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 import data from "./data";
 import List from "./componentsProjects/List";
+import AlertDialog from "./componentsProjects/AlertDialog";
 // import ErrorExample from "./components/ErrorExample";
 // import Use StateCounter from "./components/UseStateCounter";
 // import UseStateObject from "./components/UseStateObject";
@@ -12,14 +13,15 @@ function App() {
   const [people, setPeople] = useState(data);
   return (
     <main>
-      <section className="container">
-        {/* <ErrorExample /> */}
-        {/* <UseStateObject /> */}
-        {/* <UseStateCounter /> */}
-        <h3>{people.length} birthdays today</h3>
+      {/* <section className="container"> */}
+      {/* <ErrorExample /> */}
+      {/* <UseStateObject /> */}
+      {/* <UseStateCounter /> */}
+      {/* <h3>{people.length} birthdays today</h3>
         <List people={people} />
         <button onClick={() => setPeople([])}>clear all</button>
-      </section>
+      </section> */}
+      <AlertDialog />
     </main>
   );
 }
