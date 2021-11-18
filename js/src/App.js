008511fc0449ae54a20cb8f218051f0c22e56049
Kateryna Ueno <katerynaueno@gmail.com>
2021-11-18 09:40:11 +0000

    setup reviews

--- js/src/App.js
+++ js/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
-import { useState, useEffect } from "react";
-import Loading from "./componentsProjects/Loading";
-import Tours from "./componentsProjects/Tours";
+// import { useState, useEffect } from "react";
+import Review from "./componentsProjects/Review";
+
+// import Loading from "./componentsProjects/Loading";
+// import Tours from "./componentsProjects/Tours";
 
 // import data from "./data";
 // import List from "./componentsProjects/List";
@@ -10,79 +12,83 @@ import Tours from "./componentsProjects/Tours";
 // import ErrorExample from "./components/ErrorExample";
 // import Use StateCounter from "./components/UseStateCounter";
 // import UseStateObject from "./components/UseStateObject";
-const url = "https://course-api.com/react-tours-project";
+// const url = "https://course-api.com/react-tours-project";
 function App() {
   // const [people, setPeople] = useState(data);
-  const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState([]);
-
-  const removeTour = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id);
-    setTours(newTours);
-  };
-  const fetchTours = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(url);
-      const tours = await response.json();
-      setLoading(false);
-      setTours(tours);
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
-    }
-
-    console.log(tours);
-  };
-  useEffect(() => {
-    fetchTours();
-  }, []);
-  if (loading) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    );
-  }
-  if (tours.length === 0) {
-    return (
-      <main>
-        <div className="title">
-          <h2>No tours left..</h2>
-          <button className="btn" onClick={fetchTours}>
-            Refresh
-          </button>
-        </div>
-      </main>
-    );
-  }
+  // const [loading, setLoading] = useState(true);
+  // const [tours, setTours] = useState([]);
   return (
     <main>
-      <Tours tours={tours} removeTour={removeTour} />
+      <section className="container">
+        <div className="title">
+          <h2> our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
     </main>
   );
 }
+//   const removeTour = (id) => {
+//     const newTours = tours.filter((tour) => tour.id !== id);
+//     setTours(newTours);
+//   };
+//   const fetchTours = async () => {
+//     setLoading(true);
+//     try {
+//       const response = await fetch(url);
+//       const tours = await response.json();
+//       setLoading(false);
+//       setTours(tours);
+//     } catch (error) {
+//       setLoading(false);
+//       console.log(error);
+//     }
+
+//     console.log(tours);
+//   };
+//   useEffect(() => {
+//     fetchTours();
+//   }, []);
+//   if (loading) {
+//     return (
+//       <main>
+//         <Loading />
+//       </main>
+//     );
+//   }
+//   if (tours.length === 0) {
+//     return (
+//       <main>
+//         <div className="title">
+//           <h2>No tours left..</h2>
+//           <button className="btn" onClick={fetchTours}>
+//             Refresh
+//           </button>
+//         </div>
+//       </main>
+//     );
+//   }
+//   return (
+//     <main>
+//       <Tours tours={tours} removeTour={removeTour} />
+//     </main>
+//   );
+// }
 
 export default App;
 
-{
-  /* <section className="container"> */
-}
-{
-  /* <ErrorExample /> */
-}
-{
-  /* <UseStateObject /> */
-}
-{
-  /* <UseStateCounter /> */
-}
-{
-  /* <h3>{people.length} birthdays today</h3>
+/* <section className="container"> */
+
+/* <ErrorExample /> */
+
+/* <UseStateObject /> */
+
+/* <UseStateCounter /> */
+
+/* <h3>{people.length} birthdays today</h3>
         <List people={people} />
         <button onClick={() => setPeople([])}>clear all</button>
       </section> */
-}
-{
-  /* <AlertDialog /> */
-}
+
+/* <AlertDialog /> */
---
+++ js/src/componentsProjects/Review.js
@@ -0,0 +1,7 @@
+import React from "react";
+import people from "../peopleData";
+import { faChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+export default function Review() {
+  return <div></div>;
+}
