29cb0b2ca6ddf0f6740177ad4ca5d71e5b1aa390
Kateryna Ueno <katerynaueno@gmail.com>
2021-11-20 14:58:47 +0000

    finished navbar with useRef

--- navbar/src/components/NavBar.js
+++ navbar/src/components/NavBar.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
 import logo from "../logo.jpg";
 import { links, social } from "../data";
-import { AiFillTwitterCircle } from "react-icons/ai";
-import { BsFacebook } from "react-icons/bs";
 import { FaBars } from "react-icons/fa";
 const NavBar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
   return (
     <nav>
       <div className="nav-center">
@@ -18,20 +26,17 @@ const NavBar = () => {
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className="links-container show-container">
-            <ul className="links">
-              {links.map((link) => {
-                return (
-                  <li key={link.id}>
-                    <a href={link.url}>{link.text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
-
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              return (
+                <li key={link.id}>
+                  <a href={link.url}>{link.text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
         <ul className="social-icons">
           {social.map((socialLink) => {
             return (
