ed73535721a731b5681151b283ac3d109b6040d2
Kateryna Ueno <katerynaueno@gmail.com>
2021-11-26 15:31:07 +0000

    stripe like page

--- navbar/src/componentsStripe/Hero.js
+++ navbar/src/componentsStripe/Hero.js
@@ -1,7 +1,26 @@
 import React from "react";
 import { useGlobalContext } from "../stripeContext";
-export default function Hero() {
-  const data = useGlobalContext();
-  console.log(data);
-  return <div></div>;
-}
+import phone from "../images/phone.svg";
+const Hero = () => {
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Millions of businesses of all sizes – from startups to large
+            enterprises – use Stripe's software and APIs to accept payments,
+            send payouts, and manage their businesses online.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phone} className="phone-img" alt=""></img>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- navbar/src/componentsStripe/Sidebar.js
+++ navbar/src/componentsStripe/Sidebar.js
@@ -1,5 +1,44 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
+import sublinks from "../stripeData";
+import { useGlobalContext } from "../stripeContext";
 
-export default function Sidebar() {
-  return <div></div>;
-}
+const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside
+      className={`${
+        isSidebarOpen ? "sidebar-wrapper show" : "sidebar-wrapper"
+      }`}
+    >
+      <div className="sidebar">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((sublink, index) => {
+            const { page, links } = sublink;
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link, index) => {
+                    const { url, label, icon } = link;
+                    return (
+                      <a href={url} key={index}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
