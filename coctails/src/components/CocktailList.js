165680fb928e408d9dc89bfed5288a74df664c08
Kateryna Ueno <katerynaueno@gmail.com>
2021-11-29 12:03:55 +0000

    Cocktail data

--- coctails/src/components/CocktailList.js
+++ coctails/src/components/CocktailList.js
@@ -4,6 +4,13 @@ import Loading from "./Loading";
 import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
+  const { cocktail, loading } = useGlobalContext();
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktail.length < 1) {
+    return <h2 className="section-title">No cocktails matched your search</h2>;
+  }
   return (
     <div>
       <h1>CocktailList</h1>
--- coctails/src/context.js
+++ coctails/src/context.js
@@ -5,7 +5,60 @@ const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("a");
+  const [cocktail, setCocktail] = useState([]);
+
+  const fetchCocktail = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${search}`);
+      const data = await response.json();
+      console.log(data);
+      const { drinks } = data;
+      if (drinks) {
+        const drinksData = drinks.map((drink) => {
+          const {
+            idDrink,
+            strAlcoholic,
+            strCategory,
+            strDrinkThumb,
+            strDrink,
+          } = drink;
+          return {
+            id: idDrink,
+            name: strDrink,
+            category: strCategory,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+          };
+        });
+        setCocktail(drinksData);
+      } else {
+        setCocktail([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchCocktail();
+  }, [search]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        loading,
+        search,
+        cocktail,
+        setSearch,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 // make sure use
 export const useGlobalContext = () => {
