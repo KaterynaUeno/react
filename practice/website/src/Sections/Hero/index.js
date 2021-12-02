b3e62c98dd4f9c3ca89d309e387749d8fd238505
Kateryna Ueno <katerynaueno@gmail.com>
2021-12-02 15:19:42 +0000

    hero component

--- practice/website/src/Sections/Hero/index.js
+++ practice/website/src/Sections/Hero/index.js
@@ -1,7 +1,69 @@
-// This is HeroSection component, Main top section of website
-
+import "./styles.css";
+import styled from "styled-components";
+import Pink from "../../assets/blobPink.png";
+import White from "../../assets/blob white.png";
+import Purple from "../../assets/blob purple.png";
+import Arrow from "../../assets/Arrow Right.svg";
+const Blob = styled.div`
+  width: 100%;
+  position: absolute;
+  right: 0;
+`;
+const PinkBlob = styled.div`
+  width: calc(10% + 10vw);
+  position: absolute;
+  right: calc(3.5rem + 3.5vw);
+  top: calc(5rem + 5vw);
+  z-index: 6;
+`;
+const PurpleBlob = styled.div`
+  width: calc(10% + 10vw);
+  position: absolute;
+  right: 0;
+`;
+const WhiteBlob = styled.div`
+  width: calc(18% + 20vw);
+  position: absolute;
+  right: 0;
+  top: calc(10rem + 10vw);
+  z-index: 3;
+`;
 const HeroSection = () => {
-  return <div>HeroSection Component</div>;
+  return (
+    <div className="hero-section">
+      <Blob>
+        <PinkBlob>
+          <img src={Pink} alt="" width="400" height="400"></img>
+        </PinkBlob>
+        <WhiteBlob>
+          <img src={White} alt="" width="400" height="400"></img>
+        </WhiteBlob>
+        <PurpleBlob>
+          <img src={Purple} alt="" width="400" height="400"></img>
+        </PurpleBlob>
+      </Blob>
+      <div className="main-content">
+        <div className="left-block">
+          <div className="topic">
+            <div className="circle">
+              <span>Hello, we're a design company</span>
+            </div>
+          </div>
+          <div className="title">
+            Experts inWebsite & Development based in London
+          </div>
+          <div className="subtext">
+            We merge imagination and technology to help brands grow in an age of
+            digital transformation
+          </div>
+          <button className="btn">
+            Get in touch &nbsp;
+            <img src={Arrow} alt="" width="400" height="400"></img>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
--- practice/website/src/components/Header/index.js
+++ practice/website/src/components/Header/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import logoq from "../../assets/logoq-removebg-preview.png";
 import "./styles.css";
 import styled from "styled-components";
@@ -20,19 +21,21 @@ const Logo = styled.a`
   cursor: pointer;
 `;
 
-// const Nav = styled.nav`
-//   width: 25rem;
-//   max-width: 40rem;
-//   display: flex;
-//   align-items: center;
-//   justify-content: spece-between;
+const Nav = styled.nav`
+  width: 25rem;
+  max-width: 40rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-//   a {
-//     font-weight: 500;
-//     color: var(--navtext);
-//     line-height: 1.5;
-//   }
-// `;
+  a {
+    font-weight: 500;
+    color: var(--navtext);
+    line-height: 1.5;
+  }
+  @media only Screen and (max-width: 48em){
+  display: none;
+`;
 
 const Button = styled.button`
   background-color: var(--purple);
@@ -52,11 +55,14 @@ const Button = styled.button`
 `;
 const HamburgerBtn = styled.button`
   position: relative;
-  background-color: white;
+  background-color: transparent;
   width: 2rem;
   height: 2px;
   cursor: pointer;
-
+  display: none;
+@media only Screen and (max-width: 48em){
+  display: inline-block;
+}
   &::before,
   &::after {
     content: "";
@@ -67,35 +73,73 @@ const HamburgerBtn = styled.button`
     position: absolute;
     left: 0;
     cursor: pointer;
-    transition: all 0.3s;
+    transition: all 0.2s;
   }
   &::before {
-    top: 0.5rem;
-    transform: rotate(0);
+    top: ${(props) => (props.clicked ? "0" : "0.5rem")};
+    transform: ${(props) => (props.clicked ? "rotate(135deg)" : "rotate(0)")};
   }
   &::after {
-    top: 0.5rem;
-    transform: rotate(0);
+    top: ${(props) => (props.clicked ? "0" : "-0.5rem")};
+    transform: ${(props) => (props.clicked ? "rotate(-135deg)" : "rotate(0)")}
+
+`;
+const MobileMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem 0;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  background: #171f47;
+  border-radius: 20px;
+  margin: 0.5rem;
+  overflox-x: hidden;
+  opacity: ${(props) => (props.clicked ? 0.9 : 0)};
+  visibility: ${(props) => (props.clicked ? "visible" : "hidden")};
+
+  a {
+    color: white;
+    font-weight: 600;
+    font-size: 1.5rem;
+    margin: 1.5rem;
+    cursor: pointer;
   }
 `;
+
 const Header = () => {
+  const [click, setClick] = useState(false);
+  const handleClcik = () => {
+    setClick(!click);
+  };
   return (
     <Headers>
       <Logo>
         <img src={logoq} className="logo"></img>
         <h3 className="title">KatoAgency</h3>
       </Logo>
-      <nav className="nav">
+      <Nav>
         <a href="#home">Home</a>
         <a href="#services">Services</a>
         <a href="#about">About us</a>
         <a href="#contact">
           <Button>Contact us</Button>
         </a>
-      </nav>
-      <HamburgerBtn>
+      </Nav>
+      <HamburgerBtn onClick={() => handleClcik()} clicked={click}>
         <span></span>
       </HamburgerBtn>
+      <MobileMenu clicked={click}>
+        <a href="#home">Home</a>
+        <a href="#services">Services</a>
+        <a href="#about">About us</a>
+        <a href="#contact">
+          <Button>Contact us</Button>
+        </a>
+      </MobileMenu>
     </Headers>
   );
 };
