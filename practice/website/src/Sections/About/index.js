975380ff9dc242bd4c92fe33b3a7c803cba4f2fc
Kateryna Ueno <katerynaueno@gmail.com>
2021-12-13 15:11:13 +0000

    About

--- practice/website/src/Sections/About/index.js
+++ practice/website/src/Sections/About/index.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import wave from "../../assets/wave.svg";
-import hand from "../../assets/hand.svg";
+import spaceman from "../../assets/spaceman-removebg-preview.png";
+import person from "../../assets/person-removebg-preview.png";
 import styled from "styled-components";
 
 const CurvedLine = styled.div`
@@ -18,6 +19,16 @@ const About = () => {
         <h1>About Us</h1>
         <CurvedLine />
       </div>
+      <div className="rocket">
+        <img src={spaceman} alt="rocket" width="200px" height="350px" />
+      </div>
+      <div className="text">
+        <img src={person} alt="rocket" width="310px" height="280px" />
+      </div>
+      <p>
+        We started as a group of young, nerdy entrepreneurs hoping to build a
+        for-profit tech company that could be a vehicle for good.
+      </p>
     </section>
   );
 };
