f86cdf524175f634a0528d43ddcde0868e839e6d
Kateryna Ueno <katerynaueno@gmail.com>
2021-12-13 12:18:12 +0000

    About us

--- practice/website/src/Sections/About/index.js
+++ practice/website/src/Sections/About/index.js
@@ -1,12 +1,22 @@
 import "./style.css";
 import wave from "../../assets/wave.svg";
 import hand from "../../assets/hand.svg";
+import styled from "styled-components";
+
+const CurvedLine = styled.div`
+  width: 7rem;
+  height: 2rem;
+  border: 5px solid purple;
+  border-color: purple transparent transparent transparent;
+  border-radius: 170%/50px 50px;
+`;
 const About = () => {
   return (
     <section className="about">
       <img src={wave} alt="" className="wave" />
-      <div className="hand">
-        <img src={hand} alt="" className="hand" />
+      <div className="bottom-section">
+        <h1>About Us</h1>
+        <CurvedLine />
       </div>
     </section>
   );
