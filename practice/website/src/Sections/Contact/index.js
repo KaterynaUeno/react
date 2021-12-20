8e998223ea90c65ec0b3b1e85691f511fb7bdb1e
Kateryna Ueno <katerynaueno@gmail.com>
2021-12-20 15:59:27 +0000

    Contact component

--- practice/website/src/Sections/Contact/index.js
+++ practice/website/src/Sections/Contact/index.js
@@ -1,21 +1,28 @@
 import styled from "styled-components";
+import Twitter from "../../assets/twitter-square-brands.svg";
+import Facebook from "../../assets/facebook-square-brands.svg";
+import Linkedin from "../../assets/linkedin-brands.svg";
+import Instagram from "../../assets/instagram-square-brands.svg";
 
+const ContactSection = styled.div``;
+const Title = styled.h1``;
+const Icon = styled.div``;
 const Contact = () => {
   return (
     <ContactSection>
       <Title>Get in touch</Title>
       <Icon>
         <a href="https://en-gb.facebook.com/">
-          <img src="" alt="Facebook" />
+          <img src={Facebook} alt="Facebook" />
         </a>
         <a href="https://www.instagram.com/">
-          <img src="" alt="Facebook" />
+          <img src={Instagram} alt="Instagram" />
         </a>
         <a href="https://twitter.com/">
-          <img src="" alt="Facebook" />
+          <img src={Twitter} alt="Twitter" />
         </a>
         <a href="https://www.linkedin.com/">
-          <img src="" alt="Facebook" />
+          <img src={Linkedin} alt="Linkedin" />
         </a>
       </Icon>
     </ContactSection>
