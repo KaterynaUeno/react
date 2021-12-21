cdb61da10ed021766d67acdf3aad4bd62705e87f
Kateryna Ueno <katerynaueno@gmail.com>
2021-12-21 16:01:48 +0000

    Footer component

--- practice/website/src/components/Footer/index.js
+++ practice/website/src/components/Footer/index.js
@@ -1,7 +1,22 @@
-// This is footer component
-
+import styled from "styled-components";
+import Github from "../../assets/GitHub-Mark-32px.png";
+import Linkedin from "../../assets/linkedin-brands.svg";
+const Bottom = styled.div``;
+const LeftText = styled.h1``;
+const RightText = styled.h1``;
 const Footer = () => {
-  return <div>Footer Component</div>;
+  return (
+    <Bottom>
+      <LeftText>Kateryna Ueno</LeftText>
+      <RightText>Find me </RightText>
+      <a href="https://github.com/KaterynaUeno">
+        <img src={Github} alt="Github" />
+      </a>
+      <a href="https://www.linkedin.com/in/kateryna-ueno/">
+        <img src={Linkedin} alt="Github" />
+      </a>
+    </Bottom>
+  );
 };
 
 export default Footer;
