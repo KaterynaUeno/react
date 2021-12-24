bb24444244b178bf96b42981b7d4ff3fe5386bbe
Kateryna Ueno <katerynaueno@gmail.com>
2021-12-24 14:55:12 +0000

    media

--- practice/website/src/components/Header/index.js
+++ practice/website/src/components/Header/index.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import logoq from "../../assets/logoq-removebg-preview.png";
 import "./styles.css";
 import styled from "styled-components";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 const Headers = styled.header`
   display: flex;
   justify-content: space-between;
@@ -117,8 +119,54 @@ const Header = () => {
   const handleClcik = () => {
     setClick(!click);
   };
+  const ref = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const element = ref.current;
+    const mq = window.matchMedia("(max-width: 40em)");
+
+    if (mq.matches) {
+      gsap.to(element, {
+        position: "fixed",
+        top: "0",
+        left: "0",
+        right: "0",
+        padding: "1rem 2.5rem",
+        borderRadius: " 0 0 50px 50px",
+        border: "2px solid white",
+        duration: 1,
+        ease: "power1.out",
+
+        scrollTrigger: {
+          trigger: element,
+          start: "bottom +=200 top",
+          end: "+=100",
+          scrub: true,
+        },
+      });
+    } else
+      gsap.to(element, {
+        position: "fixed",
+        top: "1rem",
+        left: "3rem",
+        right: "3rem",
+        padding: "1.5rem 2rem",
+        borderRadius: "50px",
+        border: "3px solid white",
+        duration: 1,
+        ease: "power1.out",
+
+        scrollTrigger: {
+          trigger: element,
+          start: "bottom +=300 top",
+          end: "+=250",
+          scrub: true,
+        },
+      });
+  }, []);
   return (
-    <Headers>
+    <Headers ref={ref}>
       <Logo>
         <img src={logoq} className="logo" alt=""></img>
         <h3 className="title">KatoAgency</h3>
