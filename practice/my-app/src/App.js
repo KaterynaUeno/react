d52d853ba20a7e6395c8ef9959d9d71d325084eb
Kateryna Ueno <katerynaueno@gmail.com>
2021-12-30 16:13:52 +0200

    Setup

--- practice/my-app/src/App.js
+++ practice/my-app/src/App.js
@@ -1,13 +1,27 @@
+import { Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import "./App.css";
 import { lightTheme } from "./components/Themes";
 import GlobalStyle from "./globalStyles";
+import Main from "./components/Main";
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Work from "./components/Work";
+import MySkills from "./components/MySkills";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={lightTheme}>App file</ThemeProvider>
+      <ThemeProvider theme={lightTheme}>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/blog" component={Blog} />
+          <Route exact path="/work" component={Work} />
+          <Route exact path="/skills" component={MySkills} />
+        </Switch>
+      </ThemeProvider>
     </>
   );
 }
