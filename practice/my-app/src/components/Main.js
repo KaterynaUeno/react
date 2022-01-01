cda5621468a537ef64c248f582e4cf92f538829f
Kateryna Ueno <katerynaueno@gmail.com>
2022-01-01 14:02:44 +0200

    Main page component

--- practice/my-app/src/components/Main.js
+++ practice/my-app/src/components/Main.js
@@ -1,7 +1,28 @@
 import React from "react";
+import "./main.css";
+import styled from "styled-components";
 
+const MainContainer = styled.div`
+background: &{props => props.theme.body};
+width: 100vw;
+height: 100vh;
+overflow: hidden;
+position: relative;
+
+h2,h3,h4,h5.h6{
+  font-family: "Karla", sans-serif;
+  font-weight: 500;
+}`;
+
+const Container = styled.div`
+  padding: 2rem;
+`;
 const Main = () => {
-  return <div>Main Component</div>;
+  return (
+    <MainContainer>
+      <Container>Main Component</Container>
+    </MainContainer>
+  );
 };
 
 export default Main;
