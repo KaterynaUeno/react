408bdb83940eb144a84f412c03d893f152e1c46d
Kateryna Ueno <katerynaueno@gmail.com>
2022-01-04 14:11:21 +0200

    Home page

--- practice/my-app/src/components/Main.js
+++ practice/my-app/src/components/Main.js
@@ -31,6 +31,26 @@ const Contact = styled(NavLink)`
   z-index: 1;
   cursor: pointer;
 `;
+const BLOG = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  right: calc(1rem + 2vw);
+  transform: rotate(90deg) translate(-50%, -50%);
+  text-decoration: none;
+  z-index: 1;
+  cursor: pointer;
+`;
+
+const Work = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  left: calc(1rem + 2vw);
+  top: 50%;
+  text-decoration: none;
+  z-index: 1;
+  cursor: pointer;
+`;
 
 const Main = () => {
   return (
@@ -45,6 +65,12 @@ const Main = () => {
         >
           <h3>Say hi...</h3>
         </Contact>
+        <BLOG to="/blog">
+          <h3>Blog</h3>
+        </BLOG>
+        <Work to="/work">
+          <h3>Work</h3>
+        </Work>
       </Container>
     </MainContainer>
   );
