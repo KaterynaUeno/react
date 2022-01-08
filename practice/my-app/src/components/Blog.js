cdcf58b9d5e4760320d82e405fc5b4e4e64d4303
Kateryna Ueno <katerynaueno@gmail.com>
2022-01-08 14:03:18 +0200

    Blog page

--- practice/my-app/src/components/Blog.js
+++ practice/my-app/src/components/Blog.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import img from "../assets/images/kari-shea-1SAnrIxw5OY-unsplash.jpg";
+import LogoComponent from "../subComponents/LogoComponent";
+import PowerButton from "../subComponents/PowerButton";
+import SocialIcons from "../subComponents/SocialIcons";
 const MainContainer = styled.div`
   background-image: url(${img});
   background-size: cover;
@@ -14,14 +17,18 @@ const MainContainer = styled.div`
 const Container = styled.div`
   background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.8)`};
   width: 100%;
-  height: auto;
+  height: 100vh;
   position: relative;
   padding-bottom: 5rem;
 `;
 const Blog = () => {
   return (
     <MainContainer>
-      <Container></Container>
+      <Container>
+        <LogoComponent />
+        <PowerButton />
+        <SocialIcons />
+      </Container>
     </MainContainer>
   );
 };
