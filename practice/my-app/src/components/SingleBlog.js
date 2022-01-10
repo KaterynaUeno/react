e00e8354edef673917573a1614e76c5b7358ab16
Kateryna Ueno <katerynaueno@gmail.com>
2022-01-10 15:53:12 +0200

    stuff

--- practice/my-app/src/components/SingleBlog.js
+++ practice/my-app/src/components/SingleBlog.js
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Container = styled(NavLink)`
   width: calc(10rem + 15vw);
   text-decoration: none;
   height: 20rem;
   padding: 1rem;
+  color: ${(props) => props.theme.text};
+  border: 1px solid ${(props) => props.theme.text};
+  background-filter: blur(5px);
 `;
 
 const Image = styled.div`
@@ -17,11 +21,21 @@ const Image = styled.div`
   border: 1px solid transparent;
   background-position: center center;
 `;
+
+const Title = styled.h3`
+  color: inherit;
+  padding: 0.5rem 0;
+  padding-top: 1rem;
+  font-family: "Karla", sans-serif;
+  font-weight: 700;
+  border-bottom: 1px solid ${(props) => props.theme.body};
+`;
 const SingleBlog = (props) => {
-  const [name, tags, date, imgSrc, link] = props.blog;
+  const { name, id, link, imgSrc, date, tags } = props.blog;
   return (
-    <Container target="-blank" to={{ pathname: link }}>
+    <Container target="_blank" to={{ pathname: link }}>
       <Image img={imgSrc} />
+      <Title>{name}</Title>
     </Container>
   );
 };
