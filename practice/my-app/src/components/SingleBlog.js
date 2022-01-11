6bad25b14ccd033cd2f0a937c7e34cefa7f7a318
Kateryna Ueno <katerynaueno@gmail.com>
2022-01-11 15:42:19 +0200

    Blogs

--- practice/my-app/src/components/SingleBlog.js
+++ practice/my-app/src/components/SingleBlog.js
@@ -6,11 +6,22 @@ import { motion } from "framer-motion";
 const Container = styled(NavLink)`
   width: calc(10rem + 15vw);
   text-decoration: none;
-  height: 20rem;
+  height: 26rem;
   padding: 1rem;
   color: ${(props) => props.theme.text};
   border: 1px solid ${(props) => props.theme.text};
-  background-filter: blur(5px);
+  background-filter: blur(3px);
+  box-shadow: 0 0 2px 0 rgba(0, 0, 0.2);
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  z-index: 5;
+
+  &:hover {
+    color: ${(props) => props.theme.body};
+    background-color: ${(props) => props.theme.text};
+    transition: all 0.3s ease;
+  }
 `;
 
 const Image = styled.div`
@@ -28,7 +39,19 @@ const Title = styled.h3`
   padding-top: 1rem;
   font-family: "Karla", sans-serif;
   font-weight: 700;
-  border-bottom: 1px solid ${(props) => props.theme.body};
+  border-bottom: 1px solid ${(props) => props.theme.text};
+`;
+
+const Hashtag = styled.div`
+  padding: 0.5rem 0;
+`;
+
+const Tag = styled.span`
+  padding-right: 0.5rem;
+`;
+
+const Date = styled.span`
+  padding: 0.5rem 0;
 `;
 const SingleBlog = (props) => {
   const { name, id, link, imgSrc, date, tags } = props.blog;
@@ -36,6 +59,12 @@ const SingleBlog = (props) => {
     <Container target="_blank" to={{ pathname: link }}>
       <Image img={imgSrc} />
       <Title>{name}</Title>
+      <Date>{date}</Date>
+      <Hashtag>
+        {tags.map((tag, id) => {
+          return <Tag key={id}>#{tag}</Tag>;
+        })}
+      </Hashtag>
     </Container>
   );
 };
