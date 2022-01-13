60d0d1d27a1a4561b07b93d1ccfa1a2d0db530d1
Kateryna Ueno <katerynaueno@gmail.com>
2022-01-13 15:31:54 +0200

    Skillls component

--- practice/my-app/src/components/MySkills.js
+++ practice/my-app/src/components/MySkills.js
@@ -1,7 +1,36 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme } from "./Themes";
 
+const Main = styled.div`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  border: 2px solid ${(props) => props.theme.tetx};
+  color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.body};
+  padding: 2rem;
+  width: 30vw;
+  height: 50vh;
+  z-index: 3;
+  line-height: 1.5;
+`;
 const MySkills = () => {
-  return <div>Skills</div>;
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Main>
+        <Box>1</Box>
+        <Box>2</Box>
+      </Main>
+    </ThemeProvider>
+  );
 };
 
 export default MySkills;
