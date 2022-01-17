ea9225612d0f26353bbe4f141af2b19aaa6fd75d
Kateryna Ueno <katerynaueno@gmail.com>
2022-01-17 15:16:51 +0200

    About me page

--- practice/my-app/src/components/About.js
+++ practice/my-app/src/components/About.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { darkTheme } from "./Themes";
-import { Design, Develop } from "../AllSvgs";
 import PowerButton from "../subComponents/PowerButton";
 import SocialIcons from "../subComponents/SocialIcons";
 import LogoComponent from "../subComponents/LogoComponent";
@@ -12,11 +11,31 @@ const Main = styled.div`
   width: 100vw;
   height: 100vh;
   position: relative;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
+  overflow: hidden;
 `;
 
+const Box = styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  padding: 2rem;
+  width: 50vw;
+  height: 50vh;
+  z-index: 3;
+  ine-height: 1.5;
+
+  justify-content: center;
+  align-items: center;
+  font-size: calc(1rem + 1vw);
+  background-filter: blur(5px);
+
+  position: absolute;
+  left: calc(5rem+5vw);
+  top: 10rem;
+  font-family: "Ubuntu Mono", monospace;
+  h2 {
+    font-style: italic;
+  }
+`;
 const About = () => {
   return (
     <ThemeProvider theme={darkTheme}>
@@ -25,6 +44,16 @@ const About = () => {
         <LogoComponent theme="dark" />
         <SocialIcons theme="dark" />
         <ParticleComponent theme="dark" />
+
+        <Box>
+          <h2>A little about me</h2>
+          <br></br>I am front-end developer who has graduated from the bootcamp
+          Le Wagon and who loves to create great websites.
+          <br></br>I have been studing React by myself and trying to implement
+          interesting features to make user-friendly apps.
+          <br></br>
+          You can get connected with me via social links.
+        </Box>
       </Main>
     </ThemeProvider>
   );
