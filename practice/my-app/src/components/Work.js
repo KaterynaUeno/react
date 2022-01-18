a40bcbb52cca7f1c3f65b87c789013b59c4beb6d
Kateryna Ueno <katerynaueno@gmail.com>
2022-01-18 15:37:09 +0200

    Work component

--- practice/my-app/src/components/Work.js
+++ practice/my-app/src/components/Work.js
@@ -1,7 +1,44 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./Themes";
+import PowerButton from "../subComponents/PowerButton";
+import SocialIcons from "../subComponents/SocialIcons";
+import LogoComponent from "../subComponents/LogoComponent";
+import Card from "../subComponents/Card";
+import { WorkData } from "../data/WorkData";
+
+const Main = styled.div`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Box = styled.ul`
+  position: fixed;
+  top: 12rem;
+  left: calc(10rem + 15vw);
+  height: 40vh;
+  display: flex;
+  color: white;
+`;
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Main>
+        <PowerButton />
+        <LogoComponent theme="dark" />
+        <SocialIcons theme="dark" />
+        <Box>
+          {WorkData.map((data) => (
+            <Card key={data.id} data={data} />
+          ))}
+        </Box>
+      </Main>
+    </ThemeProvider>
+  );
 };
 
 export default Work;
---
+++ practice/my-app/src/subComponents/Card.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+
+const Box = styled.li`
+  width: 16rem;
+  height: 40vh;
+  background-color: ${(props) => props.theme.text};
+  color: ${(props) => props.theme.body};
+  padding: 1.5rem 2rem;
+  margin-right: 8rem;
+  border-radius: 0 50px 0 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 1px solid ${(props) => props.theme.body};
+  transition: all 0.2s ease;
+
+  &:hover {
+    background-color: ${(props) => props.theme.body};
+    color: ${(props) => props.theme.text};
+    border: 1px solid ${(props) => props.theme.text};
+  }
+`;
+
+const Title = styled.h2`
+  font-size: calc(1em + 0.5vw);
+`;
+
+const Description = styled.div``;
+const Card = (props) => {
+  const { id, name, description } = props.data;
+  return (
+    <Box key={id}>
+      <Title>{name}</Title>
+      <Description>{description}</Description>
+    </Box>
+  );
+};
+
+export default Card;
